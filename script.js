5794bc2d947b5be6653f5a9335b3b00cd3fe7aaa
denis.onwonga@student.moringaschool.com <denis.onwonga@student.Moringaschool.com>
2022-07-20 11:20:11 +0300

    changes to html, cssand js added

---
+++ script.js
@@ -0,0 +1,16 @@
+
+ 
+  alert();
+  document.getElementById('button33').onclick = function(){
+
+    }
+
+
+  
+    const element = document.getElementById("button33");
+element.addEventListener("click", myFunction);
+
+function myFunction() {
+  document.getElementById("thank you").innerHTML = "Submitted successfully";
+ 
+}
