1ebc77b3325413c7213768265bbada2097caf944
nxoo <nxoo.dev@gmail.com>
2022-07-20 11:20:21 +0300

    navbar urls

--- navbar.js
+++ navbar.js
@@ -1,12 +1,12 @@
 let navbar = document.createElement("div")
 navbar.innerHTML = "<div class=\"navbar\">\n" +
-    "  <a href=\"#\" class=\"navbar-brand\">Highway BnB</a>\n" +
+    "  <a href=\"index.html\" class=\"navbar-brand\">Highway BnB</a>\n" +
     "  <ul class=\"mx\">\n" +
     "    <li class=\"nav-item\">\n" +
-    "      <a href=\"#\" class=\"nav-link nowrap\">Become a Host</a>\n" +
+    "      <a href=\"add-listing.html\" class=\"nav-link nowrap\">Become a Host</a>\n" +
     "    </li>\n" +
     "    <li class=\"nav-item\">\n" +
-    "      <a href=\"#\" class=\"nav-link nowrap\">Browse Listings</a>\n" +
+    "      <a href=\"browse.html\" class=\"nav-link nowrap\">Browse Listings</a>\n" +
     "    </li>\n" +
     "  </ul>\n" +
     "  <ul>\n" +
