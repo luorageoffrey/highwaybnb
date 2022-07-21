8fc834f9a3fc2d9aec894e4c40dfeeb668c4a4e4
denis.onwonga@student.moringaschool.com <denis.onwonga@student.Moringaschool.com>
2022-07-21 10:16:10 +0300

    added eventlistener

--- script.js
+++ script.js
@@ -16,4 +16,13 @@ function validate() {
     }
 }
 
+const email = document.getElementById('eMails');
 
+email.addEventListener("input", function (event) {
+  if (email.validity.typeMismatch) {
+    email.setCustomValidity("I am expecting an e-mail address!");
+    email.reportValidity();
+  } else {
+    email.setCustomValidity("");
+  }
+});
